1a670244757aedcf91d84b61ba3e5bbae91a8bf7
Kevin Nedelec <kevin.nedelec@gmail.com>
2021-11-29 11:10:04 +0100

    push login / password

---
+++ src/login.js
@@ -0,0 +1,11 @@
+
+export const LoginForm = () => {
+  const login = 'MrAnderson'
+  const password = '8068CC8DBCC9A800F8D7932EE95FDB80EE14B2497E865CF644D68A6C12AC1D1E'
+
+  return (
+    <>
+      {login} / {password}
+    </>
+  )
+}
